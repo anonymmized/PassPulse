3130a797c80b3cca83f709172c702472b456d3ff
lx-insane <lx@insane.local>
2025-05-08 22:25:22 +0300

    10.2 -> Comments were removed

--- scripts/download_lists.sh
+++ scripts/download_lists.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 
-# Настройки
 REPO_URL="https://github.com/berandal666/Passwords.git"
 PASSWORDS_DIR="passwords"
 
-# Проверка наличия git
 if ! command -v git &> /dev/null; then
     echo "[!] Git не установлен. Установите его перед продолжением."
     exit 1
 fi
 
-# Создаем директорию, если её нет
 mkdir -p "$PASSWORDS_DIR"
 
-# Проверяем, существует ли папка и содержит ли она данные
 if [ -d "$PASSWORDS_DIR/.git" ]; then
     echo "[*] Репозиторий уже существует. Обновляю его..."
     (cd "$PASSWORDS_DIR" && git pull origin master)
@@ -34,7 +30,6 @@ else
     fi
 fi
 
-# Проверка содержимого
 if [ "$(ls -A "$PASSWORDS_DIR")" ]; then
     echo "[+] Списки паролей сохранены в: $PASSWORDS_DIR"
 else
